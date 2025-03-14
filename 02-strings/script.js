97da5f976d8a0c93d2af11ee9f9532d0d4e9bb3b
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-14 16:17:31 +0100

    [FIX] - 02.Strings - #1 variable name changed

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,9 +1,9 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
-let nome = 'Giacomo'
+let myName = 'Giacomo'
 
 // Stampa il valore di nome nella console.
-console.log(nome);
+console.log(myName);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
